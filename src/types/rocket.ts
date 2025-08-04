260c57518318c53a7eadf5a6249c6c4c206fdf6f
mamantube <firmanhiday.id007@gmail.com>
2025-08-05 01:29:55 +0700

    add interface for Rocket

---
+++ src/types/rocket.ts
@@ -0,0 +1,10 @@
+export interface Rocket {
+    id: string;
+    name: string;
+    description: string;
+    flickr_images: string[];
+    country: string;
+    first_flight: string;
+    cost_per_launch: number;
+
+}
