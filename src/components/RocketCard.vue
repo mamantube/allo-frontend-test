2c61b737d5da481199a31f9866994548e30487fa
mamantube <firmanhiday.id007@gmail.com>
2025-08-05 01:30:50 +0700

    add rocket card component

---
+++ src/components/RocketCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-card>
+      <router-link :to="`/rockets/${rocket.id}`">
+        <v-carousel cycle interval="3000" hide-delimiters>
+          <template #prev></template>
+          <template #next></template>
+          <v-carousel-item
+            v-for="(image, index) in rocket.flickr_images"
+            :key="index"
+            :src="image"
+            cover
+          />
+        </v-carousel>
+      </router-link>
+      <v-card-title>{{ rocket.name }}</v-card-title>
+      <v-card-text>{{ rocket.description }}</v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Rocket } from "@/types/rocket";
+
+defineProps<{
+  rocket: Rocket;
+}>();
+</script>
